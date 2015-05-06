e8f7bf76a7f59f3d78acc8624507398012090d3c
Douglas Green <bluntsporks+git@gmail.com>
2015-05-06 13:55:15 -0400

    Changing name

--- intersect.go
+++ intersect.go
@@ -1,7 +1,7 @@
 package calc
 
-// Intx forms the intersection of a variadic number of arguments.
-func Intx(Sets ...Set) Set {
+// Intersect forms the intersection of a variadic number of arguments.
+func Intersect(Sets ...Set) Set {
 	// Count the number of sets each element appears in.
 	cnts := make(map[int]int)
 	for _, A := range Sets {
--- intersect_test.go
+++ intersect_test.go
@@ -2,12 +2,12 @@ package calc
 
 import "testing"
 
-func TestIntx(t *testing.T) {
+func TestIntersect(t *testing.T) {
 	A := ToMap([]int{1, 2, 3, 5})
 	B := ToMap([]int{2, 4, 1})
 	C := ToMap([]int{4, 1, 2, 1})
 	D := ToMap([]int{2, 1})
-	if !IsEqual(Intx(A, B, C), D) {
+	if !IsEqual(Intersect(A, B, C), D) {
 		t.Error("Expected true, got false")
 	}
 }

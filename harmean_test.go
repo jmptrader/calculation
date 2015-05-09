0cde4255fe00dabcce3558c99747287e14ad41a6
Douglas Green <bluntsporks+git@gmail.com>
2015-05-09 16:49:14 -0400

    Adding test

---
+++ harmean_test.go
@@ -0,0 +1,11 @@
+package calc
+
+import "testing"
+
+func TestHarMean(t *testing.T) {
+	exp := 3.911912457
+	act := HarMean([]float64{2.3, 3.5, 5.7, 7.9})
+	if !Near(exp, act) {
+		t.Error("Expected", exp, "got", act)
+	}
+}

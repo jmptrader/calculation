ecfe656c45fda28be3df24ccab6cb2539c097df1
Douglas Green <bluntsporks+git@gmail.com>
2015-05-09 19:10:31 -0400

    Testing mode

--- mode.go
+++ mode.go
@@ -1,16 +1,28 @@
 package calc
 
+import "sort"
+
 // Mode returns the most common value(s), if any.
 func Mode(vals []float64) []float64 {
+	// Sort the values so we can compare for near equality.
+	sort.Float64s(vals)
 	cnts := make(map[float64]int)
 	max := 0
+	prev := 0.0
 	for _, val := range vals {
+		if Near(val, prev) {
+			// Discard trivial differences between floating point values.
+			val = prev
+		} else {
+			// Only save the change once a sufficient difference has occurred.
+			prev = val
+		}
 		cnts[val]++
 		if cnts[val] > max {
 			max = cnts[val]
 		}
 	}
-	modes := make([]float64, 1, len(cnts))
+	modes := make([]float64, 0, len(cnts))
 
 	// A mode must occur more than once.
 	if max > 1 {

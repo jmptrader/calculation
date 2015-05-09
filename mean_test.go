babf31ce951761a80b4facc34fb8a055de5c5c40
Douglas Green <bluntsporks+git@gmail.com>
2015-05-09 18:56:33 -0400

    Adding test

--- mean_test.go
+++ mean_test.go
@@ -3,8 +3,8 @@ package calc
 import "testing"
 
 func TestMean(t *testing.T) {
-	exp := 1.5
-	act := Mean([]float64{1, 2})
+	exp := 4.85
+	act := Mean([]float64{2.3, 3.5, 5.7, 7.9})
 	if !Near(exp, act) {
 		t.Error("Expected", exp, "got", act)
 	}

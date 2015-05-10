ba217a7b501a5e5b067f188dddb02e437f78dc2c
Douglas Green <bluntsporks+git@gmail.com>
2015-05-10 14:47:36 -0400

    Changing name

--- rms.go
+++ rms.go
@@ -2,8 +2,8 @@ package calc
 
 import "math"
 
-// RootMeanSqr returns the root mean square (RMS).
-func RootMeanSqr(vals []float64) float64 {
+// RMS returns the root mean square (RMS).
+func RMS(vals []float64) float64 {
 	sum := 0.0
 	for _, val := range vals {
 		sum += val * val

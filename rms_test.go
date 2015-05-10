147c1fa47e842787ff2165e586ba47ecc2b60ede
Douglas Green <bluntsporks+git@gmail.com>
2015-05-10 14:28:38 -0400

    Adding test and changing name

---
+++ rms_test.go
@@ -0,0 +1,11 @@
+package calc
+
+import "testing"
+
+func TestRMS(t *testing.T) {
+	exp := 5.3018865
+	act := RMS([]float64{2.3, 3.5, 5.7, 7.9})
+	if !Near(exp, act) {
+		t.Error("Expected", exp, "got", act)
+	}
+}

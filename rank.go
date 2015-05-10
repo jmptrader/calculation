1b645991ea9dc02783f1c7253439557535d15ca5
Douglas Green <bluntsporks+git@gmail.com>
2015-05-10 14:18:55 -0400

    Fixing rank and adding test

--- rank.go
+++ rank.go
@@ -3,20 +3,17 @@ package calc
 import "sort"
 
 // Rank returns the rank of first occurrence of a number in a list in the given order.
-func Rank(val float64, vals []float64, asc bool) int {
+func Rank(num float64, vals []float64, asc bool) int {
 	sort.Float64s(vals)
-	if asc {
-		for i := 0; i < len(vals); i++ {
-			if Near(val, vals[i]) {
-				return i
-			}
-		}
-	} else {
-		for i := len(vals) - 1; i >= 0; i-- {
-			if Near(val, vals[i]) {
-				return i
+	for i, val := range vals {
+		if Near(val, num) {
+			if asc {
+				return i + 1
+			} else {
+				return len(vals) - i
 			}
 		}
 	}
+
 	return -1
 }

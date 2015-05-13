115aadf63379592e6d700af271e8d5104a059af8
Douglas Green <bluntsporks+git@gmail.com>
2015-05-13 19:40:18 -0400

    Moving isempty

--- setisempty.go
+++ setisempty.go
@@ -2,6 +2,6 @@ package calc
 
 // IsEmpty tests if set A is empty.
 // See https://en.wikipedia.org/wiki/Empty_set.
-func IsEmpty(A Set) bool {
-	return len(A) == 0
+func (setA Set) IsEmpty() bool {
+	return len(setA) == 0
 }
--- setisempty_test.go
+++ setisempty_test.go
@@ -3,16 +3,16 @@ package calc
 import "testing"
 
 func TestIsEmpty(t *testing.T) {
-	A := ToMap([]int{})
-	B := ToMap([]int{1, 2, 3})
+	setA := ToMap([]int{})
+	setB := ToMap([]int{1, 2, 3})
 
 	// Test an empty set.
-	if !IsEmpty(A) {
+	if !setA.IsEmpty() {
 		t.Error("Expected true, got false")
 	}
 
 	// Test a nonempty set.
-	if IsEmpty(B) {
+	if setB.IsEmpty() {
 		t.Error("Expected false, got true")
 	}
 }

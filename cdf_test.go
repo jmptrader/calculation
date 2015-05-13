febdb9562700d380e8f33b2fa68a7ee806ebb938
Douglas Green <bluntsporks+git@gmail.com>
2015-05-13 18:22:48 -0400

    Adding cumulative distribution function

---
+++ cdf_test.go
@@ -0,0 +1,14 @@
+package calc
+
+import "testing"
+
+func TestCDF(t *testing.T) {
+	exps := []float64{0.97724994, 0.84134474, 0.5, 0.84134474, 0.97724994}
+	vals := []float64{-2.0, -1.0, 0.0, 1.0, 2.0}
+	for i, exp := range exps {
+		act := CDF(vals[i])
+		if !Near(exp, act) {
+			t.Error("Expected", exp, "got", act)
+		}
+	}
+}

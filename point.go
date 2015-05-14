9d3c8ba9c43d4ccdf4aa6e2f54b15618bec910d1
Douglas Green <bluntsporks+git@gmail.com>
2015-05-14 05:12:30 -0400

    Splitting point

--- point.go
+++ point.go
@@ -1,16 +1,7 @@
 package calc
 
-import "math"
-
 // Point type.
 // See https://en.wikipedia.org/wiki/Point_%28geometry%29.
 type Point struct {
 	X, Y float64
-}
-
-// Dist returns the distance between two points.
-func (p1 *Point) Dist(p2 *Point) float64 {
-	a := p1.X - p2.X
-	b := p1.Y - p2.Y
-	return math.Sqrt(a*a + b*b)
 }

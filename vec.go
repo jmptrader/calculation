77267bd7145687753e92503ce49d2139036a1f18
Douglas Green <bluntsporks+git@gmail.com>
2015-05-14 05:10:58 -0400

    Making vector type

---
+++ vec.go
@@ -0,0 +1,5 @@
+package calc
+
+// Vector type.
+// See https://en.wikipedia.org/wiki/Vector.
+type Vec []float64
--- vec_dotprod_test.go
+++ vec_dotprod_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestDotProd(t *testing.T) {
 	exp := 3.0
-	vec1 := []float64{1.0, 3.0, -5.0}
-	vec2 := []float64{4.0, -2.0, -1.0}
-	act := DotProd(vec1, vec2)
+	vecA := Vec([]float64{1.0, 3.0, -5.0})
+	vecB := Vec([]float64{4.0, -2.0, -1.0})
+	act := vecA.DotProd(vecB)
 	if exp != act {
 		t.Error("Expected", exp, "got", act)
 	}

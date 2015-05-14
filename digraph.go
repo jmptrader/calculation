e6d8df1814124a4db60b29be4fac3910d52cb3bc
Douglas Green <bluntsporks+git@gmail.com>
2015-05-14 18:28:17 -0400

    Adding graph types

---
+++ digraph.go
@@ -0,0 +1,8 @@
+package calc
+
+// Digraph is a type that represents a collection of vertexes and arcs.
+// See https://en.wikipedia.org/wiki/Graph_(mathematics).
+type Digraph struct {
+	Vertexes []Vertex
+	Arcs     []Arc
+}
---
+++ graph.go
@@ -0,0 +1,8 @@
+package calc
+
+// Graph is a type that represents a collection of vertexes and edges.
+// See https://en.wikipedia.org/wiki/Graph_(mathematics).
+type Graph struct {
+	Vertexes []Vertex
+	Edges    []Edge
+}
